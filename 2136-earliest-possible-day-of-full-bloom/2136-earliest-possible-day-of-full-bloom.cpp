57a0083d590cc3a6c34fd445bece8b9e5d9b12ec
Ajaysingh703 <74978870+Ajaysingh703@users.noreply.github.com>
2023-04-15 12:32:24 +0530

    Time: 380 ms (31.06%), Space: 168.6 MB (36.67%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -11,22 +11,18 @@ public:
         int n = pt.size();
         for(int i=0;i<n;i++)
         {
-            v.push_back({pt[i],gt[i]});
+            v.push_back({gt[i],pt[i]});
         }
-        sort(v.begin(),v.end(),sortt);
+        sort(v.rbegin(),v.rend());
         int ans = 0;
         int mx_time = 0;
         for(int i=0;i<n;i++)
         {
             // cout<<v[i].first<<" "<<v[i].second<<endl;
-            ans += v[i].first;
-            mx_time = max(mx_time,ans + v[i].second);
+            ans += v[i].second;
+            mx_time = max(mx_time,ans + v[i].first);
         }
-        if(mx_time<ans)
-        ans += v[n-1].second;
-        else
-            return mx_time;
-        return ans;
+        return mx_time;
        
     }
 };
