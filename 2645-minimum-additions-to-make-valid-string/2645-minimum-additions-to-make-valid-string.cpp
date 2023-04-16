11edc57f2dfc490c781f7dd32009409c6c90a4b9
Ajaysingh703 <74978870+Ajaysingh703@users.noreply.github.com>
2023-04-16 11:10:24 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (28.57%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int addMinimum(string word) {
+        //        //
+        //  aaa a bc a bc a bc -> 6
+        //  bbb a b c   a b c   a b c --> 6
+        //  ccc ab c  ab c ab c--> 6
+        // 
+      int n = word.size();
+        string s = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc";
+      int i = 0,j=0 , cnt = 0;
+      while(i<n)
+      {
+        if(word[i] == s[j] ) {
+          i++;
+          j++;
+          continue;
+        }
+        else{
+          j++;
+          cnt++;
+        }
+      }
+      if(word[n-1] != 'c')
+      {
+        cnt += 'c' - word[n-1];
+      }
+     return cnt;
+    }
+};
