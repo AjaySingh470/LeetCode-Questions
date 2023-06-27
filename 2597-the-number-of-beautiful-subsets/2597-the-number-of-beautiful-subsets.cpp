fdabec83e976e7281ef4f874fd3361d74b178390
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-06-27 14:52:02 +0530

    Time: 23 ms (91.47%), Space: 36.8 MB (9.41%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     map<int,int> mp;
     int ans = 0;
-    int dfs(int i,vector<pair<int,int>> num,int k)
+    int dfs(int i,vector<pair<int,int>> num,int k,vector<int> &cnt)
     {
         if(i == num.size()) return 1;
-        int nottake = dfs(i+1,num,k);
+        if(cnt[i]!=-1) return cnt[i];
+        int nottake = dfs(i+1,num,k,cnt);
         int take = (1<<num[i].second) - 1;
         if(i+1<num.size() and num[i+1].first - num[i].first == k)
         {
-            take *= dfs(i+2,num,k);
+            take *= dfs(i+2,num,k,cnt);
         }
         else{
-            take *= dfs(i+1,num,k);
+            take *= dfs(i+1,num,k,cnt);
         }
         
-        return take + nottake;
+        return cnt[i] = take + nottake;
 
     }
     int beautifulSubsets(vector<int>& nums, int k) {
@@ -31,7 +32,8 @@ public:
             // map<int,int> fp;
             vector<pair<int,int>> vt(it.second.begin() , it.second.end());
             //
-            int tn = dfs(0,vt ,k);
+            vector<int> v(vt.size(),-1);
+            int tn = dfs(0,vt ,k,v);
             ans *= tn;
         }
         
