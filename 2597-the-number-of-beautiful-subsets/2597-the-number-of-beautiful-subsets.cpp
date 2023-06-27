2fdb01790552476bea8c0cbc9a0ff09845233264
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-06-27 12:56:30 +0530

    Time: 171 ms (82.77%), Space: 129.1 MB (5.51%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -2,27 +2,39 @@ class Solution {
 public:
     map<int,int> mp;
     int ans = 0;
-    void dfs(int i,vector<int> &nm,int &k,int &ans)
+    int dfs(int i,vector<pair<int,int>> num,int k)
     {
-        if(i == nm.size()) {
-               ans++; return;
-        }else
+        if(i == num.size()) return 1;
+        int nottake = dfs(i+1,num,k);
+        int take = (1<<num[i].second) - 1;
+        if(i+1<num.size() and num[i+1].first - num[i].first == k)
         {
-            if(!mp[nm[i]-k] and !mp[nm[i]+k])
-            {
-                mp[nm[i]]++;
-                dfs(i+1,nm,k,ans);
-                mp[nm[i]]--;
-            }
-            dfs(i+1,nm,k,ans);
+            take *= dfs(i+2,num,k);
         }
+        else{
+            take *= dfs(i+1,num,k);
+        }
+        
+        return take + nottake;
+
     }
     int beautifulSubsets(vector<int>& nums, int k) {
-     
-      sort(nums.begin(),nums.end());
-      vector<int> dp;
-        int ans = 0;
-    dfs(0,nums,k,ans);
-      return ans-1;
+        map<int,map<int,int>> freq;
+        int ans = 1;
+        for(auto it : nums)
+        {
+            freq[it%k][it]++;
+        }
+        for(auto it : freq)
+        {
+            int nm = it.first;
+            // map<int,int> fp;
+            vector<pair<int,int>> vt(it.second.begin() , it.second.end());
+            //
+            int tn = dfs(0,vt ,k);
+            ans *= tn;
+        }
+        
+        return ans-1;
     }
 };
