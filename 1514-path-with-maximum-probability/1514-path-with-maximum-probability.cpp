b043abc64ae992cbf6bde4f8ee991b820afd9ddb
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-06-28 09:35:50 +0530

    Time: 551 ms (5.02%), Space: 70.1 MB (23.64%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    double tot = 0;
+    void dfs(int node,double tmp,vector<int> &vis,vector<vector<pair<int,double>>> &adj,int end)
+    {
+        if(node == end)
+        {
+            
+            tot = max(tot,tmp);
+            return ;
+        }
+        // if(adj[node].empty()) return;
+        if(vis[node]) return;
+        vis[node] = 1;
+        // cout<<tmp<<" ";
+        for(auto it : adj[node])
+        {
+            if(!vis[it.first])
+            dfs((it.first) , tmp*it.second ,vis, adj , end);
+        }
+    }
+    
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<vector<pair<int,double>>> adj(n);
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});            
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+        }
+        vector<double> ans(n,0.0000);
+        ans[start] = 1.0000;
+        priority_queue<pair<int,double>> pq;
+        pq.push({start , 1.0000});
+        
+        while(!pq.empty())
+        {
+            int n = pq.size();
+            auto it = pq.top();
+            pq.pop();
+            for(auto i : adj[it.first])
+            {
+                if(ans[it.first] * i.second > ans[i.first])
+                {
+                    ans[i.first] = ans[it.first]*i.second;
+                    pq.push({i.first,i.second});
+                }
+            }
+        }
+        
+        
+        return ans[end];
+        
+    }
+};
