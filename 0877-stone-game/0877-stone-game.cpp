4a72a9b1e8ff3c77ed497a3bf8a0ae5194a18ca7
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-06-30 15:03:12 +0530

    Time: 27 ms (37.85%), Space: 8.4 MB (48.72%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[501][501];
+    int check(int i,int j,bool alice ,vector<int> &piles)
+    {
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int left = 0 ;
+        if(alice)
+        {
+            left = max(piles[i] + check(i+1 , j , !alice , piles) , piles[j] + check(i , j-1 , !alice , piles));
+        }
+        else{
+            left = min(-piles[i] + check(i+1 , j , !alice , piles) , -piles[j] + check(i , j-1 , !alice , piles));
+        }
+        return dp[i][j] = left ;
+    }
+    
+    bool stoneGame(vector<int>& piles) {
+                memset(dp,-1,sizeof(dp));
+
+        return check(0,piles.size()-1 ,true , piles);
+    }
+};
