02ff896897c689cdb6f9bd52abae6edca4cc5857
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-07-03 09:20:41 +0530

    Time: 8 ms (20.95%), Space: 7.1 MB (9.04%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -4,27 +4,46 @@ public:
         map<char,int> mp,gl;
         if(s.size() != goal.size() || s.size() == 1 || goal.size() == 1) return false;
         for(auto i : s) mp[i]++;
-        // if(s == goal) return false;
-        for(auto i: goal)
-        {
-            gl[i]++;
-            if(mp[i] == 0) return false;
-        }
-        cout<<2<<endl;
-        ///
-        if(mp.size() == 1) return true;
+        // // if(s == goal) return false;
+        // for(auto i: goal)
+        // {
+        //     gl[i]++;
+        //     if(mp[i] == 0) return false;
+        // }
+        // // cout<<2<<endl;
+        // ///
+        // // if(mp.size() == 1) return true;
+        // if(s == goal)
+        // {
+        //     for(auto i : s) if(mp[i]>1) return true;
+        //     return false;
+        // }    
+        // int cnt = 0;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     if(s[i] != goal[i]) cnt++; 
+        //     else if(mp[s[i]] != gl[goal[i]]) return false;
+        // }
+        // return cnt==2;
         if(s == goal)
         {
             for(auto i : s) if(mp[i]>1) return true;
             return false;
-        }    
-        int cnt = 0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i] != goal[i]) cnt++; 
-            else if(mp[s[i]] != gl[goal[i]]) return false;
         }
-        return cnt==2;
+        int i=-1,j=-1;
+        for(int k=0;k<goal.size();k++)
+        {
+            if(s[k] != goal[k])
+            {
+                if(i == -1) i = k;
+                else if(j == -1) j = k;
+                else return false;
+            }   
+            
+        }
+        if(i == -1 || j == -1) return false;
+        swap(s[i] , s[j]);
+        return goal == s;
         
         
     }
