2cba52eb6415743f285b157022822578fdf280a6
Ajay Singh <74978870+Ajaysingh703@users.noreply.github.com>
2023-07-03 09:13:27 +0530

    Time: 13 ms (9.76%), Space: 6.9 MB (52.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        map<char,int> mp,gl;
+        if(s.size() != goal.size() || s.size() == 1 || goal.size() == 1) return false;
+        for(auto i : s) mp[i]++;
+        // if(s == goal) return false;
+        for(auto i: goal)
+        {
+            gl[i]++;
+            if(mp[i] == 0) return false;
+        }
+        cout<<2<<endl;
+        ///
+        if(mp.size() == 1) return true;
+        if(s == goal)
+        {
+            for(auto i : s) if(mp[i]>1) return true;
+            return false;
+        }    
+        int cnt = 0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] != goal[i]) cnt++; 
+            else if(mp[s[i]] != gl[goal[i]]) return false;
+        }
+        return cnt==2;
+        
+        
+    }
+};
