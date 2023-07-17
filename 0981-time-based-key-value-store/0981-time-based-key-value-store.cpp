52271fdaef8731e49ae2edd6db4fc295582198a0
Ajay Singh <74978870+AjaySingh470@users.noreply.github.com>
2023-07-17 21:48:11 +0530

    Time: 381 ms (35.94%), Space: 132.1 MB (10.65%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -3,65 +3,20 @@ public:
     TimeMap() {
         
     }
-    
-    int search(int val,vector<pair<int,string>> &v)
-    {
-        int l = 0 , r = v.size()-1;
-        int ans = 0;
-        while(l<=r)
-        {
-            int mid = (l+r)>>1;
-            if(v[mid].first<=val)
-            {
-                ans = mid;
-                l = mid + 1;
-            }
-            else
-                r = mid-1;
-        }
-        return ans;
-    }
-    
-    map<string,vector<pair<int,string>>> time;
-    
+    map<string,map<int,string>> mp;
     void set(string key, string value, int timestamp) {
-        // key -> value with timestamp;
-        time[key].push_back({timestamp,value});
-        // val[key].push_back(value);
+        mp[key][timestamp] = value;
     }
     
     string get(string key, int timestamp) {
-         // vector<pair<int,string>> tm = time[key];
-        
-        if(time.find(key) == time.end()) return "";
-        if(timestamp < time[key][0].first)
-            return "";
-        //////////////////
-        
-        int l = 0 , r = time[key].size();
-        int val = timestamp;
-        int ns = 0;
-        while(l<r)
-        {
-            int mid = (l+r)>>1;
-            if(time[key][mid].first<=val)
-            {
-               
-                l = mid + 1;
-            }
-            else
-                r = mid;
-        }
-        /////////////////
-        int it = r;
-        if(it == 0 ) return "";
-        
-        string ans = time[key][it-1].second;
-        return ans;
+        auto it = mp[key].upper_bound(timestamp);
+        if(it == mp[key].begin()) return "";
+
+        return prev(it)->second;
     }
 };
 
-/* 
+/**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap* obj = new TimeMap();
  * obj->set(key,value,timestamp);
