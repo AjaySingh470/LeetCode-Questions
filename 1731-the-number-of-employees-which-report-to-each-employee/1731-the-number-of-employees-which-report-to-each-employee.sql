d2a58bf504458f3e3c0e11a4c1ea92311fa31be7
Ajay Singh <74978870+AjaySingh470@users.noreply.github.com>
2023-08-23 21:46:13 +0530

    Time: 2023 ms (8.22%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,5 @@
-select reports_to as employee_id ,
-    (select e.name from employees as e where e2.reports_to = e.employee_id) as name,
-    count(employee_id) as reports_count , 
-    round(avg(age),0) as average_age
-from employees as e2
-group by reports_to 
-having reports_to in (select employee_id from Employees)
-order by employee_id
-
-;
+select e2.reports_to as employee_id , e1.name , count(e2.employee_id) as reports_count , round(avg(e2.age)) as average_age
+from employees as e1 join employees as e2
+on e1.employee_id = e2.reports_to
+group by e2.reports_to
+order by e2.reports_to ;
