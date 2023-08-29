e79623a18562a5e85ef7854af9b7769ec37b050e
Ajay Singh <74978870+AjaySingh470@users.noreply.github.com>
2023-08-29 11:21:22 +0530

    Time: 24 ms (80.18%), Space: 10.6 MB (86.10%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,34 +2,22 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int y = 0 , n = 0;
-        for(auto it : customers)
-        {
-            if(it == 'Y')
-                y++;
-        }
-        // if(y == customers.size()) return customers.size();
-        vector<int> mp(1e5+1,-1);
-        int mn = INT_MAX,idx = -1;
+        int mn = 0 , curr = 0;
+        int hr = 0;
         for(int i=0;i<customers.size();i++)
         {
-            int score = y + n;
-            if(customers[i] == 'N') n++;
-            else y--;
-            if(mp[score]==-1)
-            { 
-                mp[score] = i;
-                mn = min(mn,score);
+            char ch = customers[i];
+            if(ch == 'Y')
+            {
+                curr--;
+            }
+            else curr++;
+            if(curr < mn)
+            {
+                mn = curr;
+                hr = i+1;
             }
         }
-        int score = y + n;
-        if(mp[score] == -1)
-        {
-            mp[score] = customers.size();
-            mn = min(mn,score);
-
-        }
-        // auto it = upper_bound(mp.begin(),mp.end(),-1);
-        return mp[mn];
-        
+        return hr;
     }
 };
