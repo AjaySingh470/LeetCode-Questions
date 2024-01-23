fea1ce2b753e09848c3006b8c16c830894f85582
Ajay Singh <74978870+AjaySingh470@users.noreply.github.com>
2024-01-23 12:32:21 +0530

    Time: 584 ms (10.03%), Space: 326.9 MB (6.62%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-679cca-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1239-maximum-length-of-a-concatenated-string-with-unique-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-679cca-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    int ans = 0;
+    
+    bool hasunique(string &s)
+    {
+        map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            if(mp[s[i]]>1) return false;
+        }
+        return true;
+    }
+    
+    void recur(int i,vector<string>& arr,string s)
+    {
+        if(i == arr.size())
+        {
+            if(hasunique(s))
+            {
+                ans = max(ans , (int)s.size());
+            }
+            return;
+        }
+        // nottake
+        recur(i+1, arr, s);
+        s += arr[i];
+        if(hasunique(s))
+        {
+            recur(i+1,arr,s);
+        }
+        
+    }    
+        
+    
+    int maxLength(vector<string>& arr) {
+        ans = 0;
+        recur(0,arr,"");
+        return ans;
+    }
+};
